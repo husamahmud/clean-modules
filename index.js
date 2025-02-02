aa09fc5f35929feb109a092919113dafc1c513f4
husamahmud <husamahmud@gmail.com>
2025-02-02 22:27:43 +0300

    add mod & sum and index

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const { exec } = require('child_process')
+const path = require('path')
+
+const binaryPath = path.join(__dirname, 'bin', 'clean-modules')
+
+exec(binaryPath, (err, stdout, stderr) => {
+  if (err) {
+    console.error(`Error: ${stderr}`)
+    process.exit(1)
+  }
+  console.log(stdout)
+})
