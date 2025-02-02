3d6aef0fb8ff60a8eee3b74e249c840ccce1b971
husamahmud <husamahmud@gmail.com>
2025-02-02 22:56:53 +0300

    update binary bath

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
-const { exec } = require('child_process')
+const { spawn } = require('child_process')
 const path = require('path')
 
 const binaryPath = path.join(__dirname, 'bin', 'drop-modules')
 const currentDir = process.cwd()
 
-exec(binaryPath, {
-  cwd: currentDir,
+const child = spawn(binaryPath, [currentDir], {
   stdio: 'inherit',
-}, (err, stdout, stderr) => {
-  if (err) {
-    console.error('Error executing binary:', err)
-    console.error(`stderr: ${stderr}`)
-    process.exit(1)
-  }
-  if (stdout) console.log(stdout)
+})
+
+child.on('error', (err) => {
+  console.error('Failed to start subprocess.', err)
+  process.exit(1)
+})
+
+child.on('close', (code) => {
+  process.exit(code)
 })
